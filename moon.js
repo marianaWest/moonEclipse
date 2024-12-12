0bcd984633e91be4bf4b5ad3218675ea89939a89
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2024-12-12 10:09:45 +0000

    Trying to delete previous image from canvas

--- moon.js
+++ moon.js
@@ -32,46 +32,47 @@ const sketch = ({ context, width, height }) => {
 
     typeContext.save();
     typeContext.drawImage(currentImage, 0, 0, cols, rows);
-    typeContext.restore();
+    // typeContext.restore();
 
-    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+    // const typeData = typeContext.getImageData(0, 0, cols, rows).data;
 
-    context.fillStyle = "black";
-    context.fillRect(0, 0, width, height);
+    // context.fillStyle = "black";
+    // context.fillRect(0, 0, width, height);
 
-    for (let i = 0; i < numCells; i++) {
-      const col = i % cols;
-      const row = Math.floor(i / cols);
+    // for (let i = 0; i < numCells; i++) {
+    //   const col = i % cols;
+    //   const row = Math.floor(i / cols);
 
-      const x = col * cell + random.range(-cell, cell) * 0.5;
-      const y = row * cell + random.range(-cell, cell) * 0.5;
+    //   const x = col * cell + random.range(-cell, cell) * 0.5;
+    //   const y = row * cell + random.range(-cell, cell) * 0.5;
 
-      const r = typeData[i * 4 + 0];
-      const g = typeData[i * 4 + 1];
-      const b = typeData[i * 4 + 2];
-      const a = typeData[i * 4 + 3];
+    //   const r = typeData[i * 4 + 0];
+    //   const g = typeData[i * 4 + 1];
+    //   const b = typeData[i * 4 + 2];
+    //   const a = typeData[i * 4 + 3];
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b}, ${a})`;
+    //   context.fillStyle = `rgb(${r}, ${g}, ${b}, ${a})`;
 
-      context.save();
-      context.translate(x, y);
-      context.transform(1, 0.1, 0.8, 0.8, 0, 0);
+    //   context.save();
+    //   context.translate(x, y);
+    //   context.transform(1, 0.1, 0.8, 0.8, 0, 0);
 
-      context.beginPath();
-      context.arc(0, 0, cell, Math.PI * 1.52, Math.PI * 0.5, true);
-      context.arc(10, 0, cell, Math.PI * 0.58, Math.PI * 1.4);
-      context.closePath();
-      context.fill();
+    //   context.beginPath();
+    //   context.arc(0, 0, cell, Math.PI * 1.52, Math.PI * 0.5, true);
+    //   context.arc(10, 0, cell, Math.PI * 0.58, Math.PI * 1.4);
+    //   context.closePath();
+    //   context.fill();
 
-      context.restore();
-      context.fillSyle = "black";
-    }
+    //   context.restore();
+    // context.fillSyle = "black";
+    // }
     // this line draws the image obtained from picsum on the top left corner
-    // context.drawImage(typeCanvas, 0, 0);
+    context.drawImage(typeCanvas, 0, 0);
   };
 };
 
 loadMeSomeImage = (url) => {
+  console.log("calls loadmesomeimage");
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.crossOrigin = "Anonymous";
@@ -83,17 +84,8 @@ loadMeSomeImage = (url) => {
 
 const start = async () => {
   try {
-    if (currentImage) {
-      context.fillStyle = "black";
-      await manager.unload();
-    }
     // Load a new image
     currentImage = await loadMeSomeImage(url);
-    // Clear the previous sketch
-    if (manager) {
-      await manager.unload();
-    }
-
     // Start a new sketch with the updated settings
     manager = await canvasSketch(sketch, settings);
   } catch (err) {
@@ -103,9 +95,10 @@ const start = async () => {
 
 // Run the `start` function every 2 seconds to fetch a new image
 // not working
-setInterval(() => {
-  start();
-}, 2000);
+//  setInterval(() => {
+//   console.log('calls setInterval')
+//   loadMeSomeImage(url);
+//  }, 2000);
 
 // Start the first sketch
 start();
